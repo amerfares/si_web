acee566fb00c4dd6308d41709d937d4bb9173663
amerfares <faresamer.pro@gmail.com>
2023-01-03 13:42:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const endpointUrl = 'https://query.wikidata.org/sparql';
+
+const sparqlQuery = `SELECT DISTINCT ?country ?countryLabel ?capital ?capitalLabel
+WHERE
+{
+  ?country wdt:P31 wd:Q3624078 .
+  #not a former country
+  FILTER NOT EXISTS {?country wdt:P31 wd:Q3024240}
+  #and no an ancient civilisation (needed to exclude ancient Egypt)
+  FILTER NOT EXISTS {?country wdt:P31 wd:Q28171280}
+  OPTIONAL { ?country wdt:P36 ?capital } .
+
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "fr" }
 }
+ORDER BY ?countryLabel`;
 
-export default App;
+
+
+function App() { 
+
+  const [data, setData] = useState();
+  useEffect(() => {
+    async function query () {
+      const fullUrl = endpointUrl + '?query=' + encodeURIComponent( sparqlQuery );
+      const headers = { 'Accept': 'application/sparql-results+json' };
+      const response = await fetch(fullUrl, { headers });
+      const data_aw = await response.json();
+      setData(data_aw.results.bindings);    
+  }
+  query();
+    }  
+, []);
+
+return (
+  
+  <div>
+    {data ? data.map(item => (
+      <div key={item.country.value}>
+        <p>{item.countryLabel ? item.countryLabel.value : 'pas de valeur'}</p>
+        <p>{item.capitalLabel ? item.capitalLabel.value : 'pas de valeur'}</p>
+      </div>
+    )) : 'Loading...'}
+  </div>
+);
+  }
+
+  export default App;
